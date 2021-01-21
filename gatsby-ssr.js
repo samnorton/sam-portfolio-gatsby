fa6d4015995643ee61f4ab6b777d095b6339f750
Sam Norton <samuelnico.norton@gmail.com>
2021-01-21 13:10:47 +0800

    Remove jQuery

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,25 +2,9 @@ import React from 'react'
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
-      crossOrigin="anonymous"
-      defer
-    />,
-    <script
-      key="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-      crossOrigin="anonymous"
-      defer
-    />,
-    <script
-      key="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-      crossOrigin="anonymous"
-      defer
-    />,
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
+     integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" 
+     crossorigin="anonymous" />,
       <script
       src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"
       crossOrigin="anonymous"
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -27,11 +27,6 @@ const Layout = ({ children }) => {
 
   return (
      <>
-      <Helmet>
-          <script src={withPrefix('owl.carousel.min.js')} type="text/javascript" />
-          <script src={withPrefix('sticky-menu.js')} type="text/javascript" />
-          <script src={withPrefix('script.js')} type="text/javascript" />
-      </Helmet>
      <Navbar />
      { children}
      <Footer />
